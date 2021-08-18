3d0e661badaab5855c4beea133a73e59e2031efe
L1atte <1454841541@qq.com>
2021-08-19 01:35:59 +0800

    1.完成虚拟列表

---
+++ src/components/item.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Author: Latte
+ * @Date: 2021-08-19 00:21:52
+ * @LAstEditors: Latte
+ * @LastEditTime: 2021-08-19 01:18:46
+ * @FilePath: \virtual-list\src\components\item.vue
+-->
+<template>
+  <div style="height: 40px; border:1px solid red">
+    {{item.value}}
+  </div>
+</template>
+
+<script>
+
+export default ({
+  props: {
+    item: Object,
+  },
+  created() {
+    console.log("312312",this.item)
+  }
+}) 
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Latte
+ * @Date: 2021-08-18 23:28:59
+ * @LAstEditors: Latte
+ * @LastEditTime: 2021-08-19 00:02:02
+ * @FilePath: \virtual-list\src\main.js
+ */
+import { createApp } from "vue";
+import App from "./App.vue";
+// import router from "./router";
+
+createApp(App).mount("#app");
