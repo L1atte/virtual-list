6661788d7bfbacd3164fe80a5561508376344d97
L1atte <59164539+L1atte@users.noreply.github.com>
2021-08-19 16:27:48 +0800

    Add files via upload

--- src/components/item.vue
+++ src/components/item.vue
@@ -2,23 +2,19 @@
  * @Author: Latte
  * @Date: 2021-08-19 00:21:52
  * @LAstEditors: Latte
- * @LastEditTime: 2021-08-19 01:18:46
+ * @LastEditTime: 2021-08-19 14:02:12
  * @FilePath: \virtual-list\src\components\item.vue
 -->
 <template>
-  <div style="height: 40px; border:1px solid red">
-    {{item.value}}
+  <div style="border: 1px solid red; padding: 20px 0">
+    {{ item.id }} {{ item.value }}
   </div>
 </template>
 
 <script>
-
-export default ({
+export default {
   props: {
     item: Object,
   },
-  created() {
-    console.log("312312",this.item)
-  }
-}) 
+};
 </script>
